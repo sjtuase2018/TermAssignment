5543d2eb4302c4c96c5a5a3208b71d367313175d
fallawk <43328444+fallawk@users.noreply.github.com>
2018-10-12 11:35:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+CSRF_ENABLED = True
+SECRET_KEY = 'show me the money'
+
