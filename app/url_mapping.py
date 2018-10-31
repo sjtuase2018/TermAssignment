34722ab4e3d3431226259b2f8d32e227200be02f
fallawk <43328444+fallawk@users.noreply.github.com>
2018-10-31 16:48:42 +0800

    Update url_mapping.py

--- app/url_mapping.py
+++ app/url_mapping.py
@@ -1,84 +1,111 @@
-from app import app, db
-from flask_login import login_required, current_user, login_user, logout_user
-from flask import render_template, redirect, url_for, flash, request
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
-from werkzeug.urls import url_parse
-from app.entity_mapping import User
+# -*- coding: UTF-8 -*-
+from flask import request, Blueprint, jsonify
+from db_io import ExitUser, VarifyUser, LogQuery, GetWeekDayLogNum, GetLogNumByRule, GetAreas, GetLogNumByArea, GethazardCategoryByArea,\
+    AreaUpdate, NewArea, DeleteArea
 
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    return render_template('index.html', title='HomePage')
+api = Blueprint('api', __name__)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@api.route('/login/', methods=('POST',))
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    class LoginForm(FlaskForm):
-        username = StringField('UserName', validators=[DataRequired()])
-        password = PasswordField('Password', validators=[DataRequired()])
-        remember_me = BooleanField('remember_me', default=False)
-        submit = SubmitField('Sign In')
-
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        u = User.query.filter_by(username=form.username.data).first()
-        if u is None or not u.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('index'))
-        login_user(u, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form, can_reg=True)
+    username = request.json['username']
+    password = request.json['password']
+    if ExitUser(username):
+        id = VarifyUser(username, password)
+        if id is not None:
+            return jsonify({'code': 200, 'username': username, 'userId': id})
+    return jsonify({'code': 400})
 
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+@api.route('/getCamera/', methods=('GET',))
+def getCamera():
+    dict_areas = GetAreas()
+    list_re = []
+    for x in dict_areas.keys():
+        dict_tmp = GethazardCategoryByArea(x)
+        dict_toAppend = {}
+        dict_toAppend['id'] = x
+        dict_toAppend['area'] = dict_areas[x]
+        dict_toAppend['hazardCategory'] = ''
+        for y in dict_tmp.keys():
+            dict_toAppend['hazardCategory'] += (dict_tmp[y] + ',')
+        list_re.append(dict_toAppend)
+
+    return jsonify(list_re)
 
 
-@app.route('/user/<username>')
-@login_required
-def userprofile(username):
-    u = User.query.filter_by(username=username).first_or_404()
-    # first_or_404() implement by flask_sqlalchemy, raising 404 automatically instead of returning none
-    return render_template('user.html', user=u, title='UserProfile')
+@api.route('/getLogs/', methods=('GET','POST'))
+def getLogs():
+    data = request.get_json()
+    # print data
+    return jsonify(LogQuery(data['sortby'], data['pagesize'], data['startwith']))
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
+@api.route('/getChart/', methods=('GET', 'POST'))
+def getChart():
+    data = request.get_json()
+    # stdate = data['startTime']
+    # enddate = data['endTime']
+    stdate = '23233'
+    enddate = '2323233'
+    list_weekday_num = GetWeekDayLogNum(stdate, enddate)
+    list_num_by_rule_1 = GetLogNumByRule(stdate, enddate, 1)
+    list_num_by_rule_2 = GetLogNumByRule(stdate, enddate, 2)
+    list_num_by_rule_3 = GetLogNumByRule(stdate, enddate, 3)
 
-    class RegForm(FlaskForm):
-        username = StringField('Username', validators=[DataRequired()])
-        password = PasswordField('Password', validators=[DataRequired()])
-        password2 = PasswordField(
-            'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-        submit = SubmitField('Register')
+    dict_area = GetAreas()
+    list_seriesData = []
+    list_names = []
 
-        def validate_username(self, username):
-            u = User.query.filter_by(username=username.data).first()
-            if u is not None:
-                raise ValidationError('Please use a different username.')
+    for x in dict_area.keys():
+        dict_tmp = {}
+        dict_tmp['name'] = dict_area[x]
+        dict_tmp['value'] = GetLogNumByArea(stdate, enddate, x)
+        list_seriesData.append(dict_tmp)
+        list_names.append(dict_area[x])
 
-    form = RegForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    dict_re = {}
+    dict_re['dataWeek'] = list_weekday_num
+    dict_re['dataM1'] = list_num_by_rule_1
+    dict_re['dataM2'] = list_num_by_rule_2
+    dict_re['dataM3'] = list_num_by_rule_3
+    dict_re['seriesData'] = list_seriesData
+    dict_re['name'] = list_names
+
+    return jsonify(dict_re)
+
+
+@api.route('/settingSave/', methods=('GET', 'POST'))
+def settingSave():
+    data = request.get_json()
+    areaId = date['id']
+    areaName = data['area']
+    rules = data['rules'] # list
+    if not areaId is None:
+        AreaUpdate(areaId, name=areaName, rule_list=rules)
+        return jsonify({'code': 200})
+    else:
+        return jsonify({'code': 400})
+
+
+@api.route('/settingNew/', methods='POST')
+def settingNew():
+    data = request.get_json()
+    areaName = data['area']
+    appliedRules = data['rules'] # list
+    if NewArea(areaName, appliedRules):
+        return jsonify({'code': 200})
+    else:
+        return jsonify({'code', 400})
+
+    
+@api.route('/settingDelete/', methods=('POST',))
+def settingDelete():
+    data = request.get_json()
+    areaId = data['id']
+    if DeleteArea(areaId):
+        return jsonify({'code': 200})
+    else:
+        return 400
 
