c5e36d95fcb7e8ef6d18ee116d514100df905ef3
fallawk <43328444+fallawk@users.noreply.github.com>
2018-11-11 08:35:34 +0800

    Update video_stream.py

--- app/video_stream.py
+++ app/video_stream.py
@@ -6,23 +6,14 @@ from flask import Response
 
 def gen(came):
     while True:
-        frame = came.get_frame()
-        if not frame is None:
+        frame = came.get_frame('jpeg')
+        if frame is not None:
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
         else:
+            print('no signal')
             break
-
-#
-# @app.route('/video_feed/<camid>')
-# def video_feed(camid):
-#     return Response(gen(Camera(camid)), mimetype='multipart/x-mixed-replace; boundary=frame')
-#
-#
-# @app.route('/video_test')
-# def video_test():
-#     return render_template('video_feed.html')
-
+        
 
 @api.route('/video_feed/')
 def video_feed():
