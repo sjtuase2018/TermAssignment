2ba026e130a72d896898ee2050419510963315b5
fallawk <43328444+fallawk@users.noreply.github.com>
2018-12-28 10:54:02 +0800

    初始化检测器

--- signal_processor/__init__.py
+++ signal_processor/__init__.py
@@ -1,7 +1,9 @@
-from keras_yolo3.yolo import YOLO
+from keras_yolo3.yolo import YOLO as FIGURE_YOLO
+from yolo3_helmet_detection.YOLO import YOLO as HELMET_YOLO
+
 
 print('initing all model processors')
-yolo_figure_detector = YOLO()
-
+yolo_figure_detector = FIGURE_YOLO()
+yolo_helmet_detector = HELMET_YOLO()
 
 print('all processors ready')
